5c9e06468f735f727145321134740a49cc5522d5
Amber Singh <ambersingh365@gmail.com>
2026-09-19 11:37:04 +0530

    Practiced Spring Security Basics

---
+++ 23-spring-security-demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @GetMapping
+    public ResponseEntity<String> getStudents() {
+        return ResponseEntity.ok("Student retrieved successfully");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createStudents() {
+        return ResponseEntity.ok("Student created successfully");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteStudents() {
+        return ResponseEntity.ok("Student deleted successfully");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateStudents() {
+        return ResponseEntity.ok("Student updated successfully");
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getToken(CsrfToken csrfToken) {
+        return csrfToken;
+    }
+}
